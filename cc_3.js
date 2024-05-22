ad1ad5a06c3cb61e9d304dd7a19ba1b3f4150533
Bhoang02 <brianhoang@usf.edu>
2024-05-21 23:09:20 -0400

    Create Car class with constructor for make and speed.

---
+++ cc_3.js
@@ -0,0 +1,14 @@
+// U89173488
+
+// Step 1: Implement the Car class with a constructor
+
+class Car {
+    constructor(make, speed) {
+      this.make = make;
+      this.speed = speed;
+    }
+
+    
+    }
+
+  module.exports = Car;
